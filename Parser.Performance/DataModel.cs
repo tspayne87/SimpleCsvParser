52e6191d2ce74027fd4e46903adcfa17e3326f2b
Justin_H_NOTADMIN <justin@anjusevin.com>
2022-05-25 09:19:13 -0500

    Change name; Some crazy guy was just trying to confuse people

--- Parser.Performance/DataModel.cs
+++ Parser.Performance/DataModel.cs
@@ -10,78 +10,78 @@ namespace Parser.Performance
     {
         [CsvProperty(0)]
         [Index(0)]
-        public string Row1 { get; set; }
+        public string Column1 { get; set; }
         [CsvProperty(1)]
         [Index(1)]
-        public string Row2 { get; set; }
+        public string Column2 { get; set; }
         [CsvProperty(2)]
         [Index(2)]
-        public string Row3 { get; set; }
+        public string Column3 { get; set; }
         [CsvProperty(3)]
         [Index(3)]
-        public string Row4 { get; set; }
+        public string Column4 { get; set; }
         [CsvProperty(4)]
         [Index(4)]
-        public string Row5 { get; set; }
+        public string Column5 { get; set; }
         [CsvProperty(5)]
         [Index(5)]
-        public string Row6 { get; set; }
+        public string Column6 { get; set; }
         [CsvProperty(6)]
         [Index(6)]
-        public string Row7 { get; set; }
+        public string Column7 { get; set; }
         [CsvProperty(7)]
         [Index(7)]
-        public string Row8 { get; set; }
+        public string Column8 { get; set; }
         [CsvProperty(8)]
         [Index(8)]
-        public string Row9 { get; set; }
+        public string Column9 { get; set; }
         [CsvProperty(9)]
         [Index(9)]
-        public string Row10 { get; set; }
+        public string Column10 { get; set; }
         [CsvProperty(10)]
         [Index(10)]
-        public string Row11 { get; set; }
+        public string Column11 { get; set; }
         [CsvProperty(11)]
         [Index(11)]
-        public string Row12 { get; set; }
+        public string Column12 { get; set; }
         [CsvProperty(12)]
         [Index(12)]
-        public string Row13 { get; set; }
+        public string Column13 { get; set; }
         [CsvProperty(13)]
         [Index(13)]
-        public string Row14 { get; set; }
+        public string Column14 { get; set; }
         [CsvProperty(14)]
         [Index(14)]
-        public string Row15 { get; set; }
+        public string Column15 { get; set; }
         [CsvProperty(15)]
         [Index(15)]
-        public string Row16 { get; set; }
+        public string Column16 { get; set; }
         [CsvProperty(16)]
         [Index(16)]
-        public string Row17 { get; set; }
+        public string Column17 { get; set; }
         [CsvProperty(17)]
         [Index(17)]
-        public string Row18 { get; set; }
+        public string Column18 { get; set; }
         [CsvProperty(18)]
         [Index(18)]
-        public string Row19 { get; set; }
+        public string Column19 { get; set; }
         [CsvProperty(19)]
         [Index(19)]
-        public string Row20 { get; set; }
+        public string Column20 { get; set; }
         [CsvProperty(20)]
         [Index(20)]
-        public string Row21 { get; set; }
+        public string Column21 { get; set; }
         [CsvProperty(21)]
         [Index(21)]
-        public string Row22 { get; set; }
+        public string Column22 { get; set; }
         [CsvProperty(22)]
         [Index(22)]
-        public string Row23 { get; set; }
+        public string Column23 { get; set; }
         [CsvProperty(23)]
         [Index(23)]
-        public string Row24 { get; set; }
+        public string Column24 { get; set; }
         [CsvProperty(24)]
         [Index(24)]
-        public string Row25 { get; set; }
+        public string Column25 { get; set; }
     }
 }
